940da5b550d83e457ad3c3074c6d37be05dca5f8
Ilias Ismanalijev <hello@illyism.com>
2014-11-12 15:52:00 +0100

    form basics

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var recent = require("../controller/recent")
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: "Avalon: The Net's First Online Text Based Roleplaying Game", recent: recent() });
+  res.render('index', { title: "Avalon: The Net's First Online Text Based Roleplaying Game" });
 });
 
 module.exports = router;
