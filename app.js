062349fe823e866851267766e8208e657aa4131d
Ilias Ismanalijev <hello@illyism.com>
2014-11-13 09:54:06 +0100

    etc

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/php')));
 
 app.use('/', routes);
 app.use('/intro', intro);
@@ -60,6 +61,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
