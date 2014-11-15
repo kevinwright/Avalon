90047800379d6eca3e70c09c8d044a910720d234
Ilias Ismanalijev <hello@illyism.com>
2014-11-15 01:15:23 +0100

    canonicals, seo

--- routes/help.js
+++ routes/help.js
@@ -4,7 +4,9 @@ var router = express.Router();
 var help = require("../controller/help");
 
 router.get('/', help.index);
+router.get('/sections/:section.html', help.section);
 router.get('/sections/:section', help.section);
+router.get('/pages/:page.html', help.page);
 router.get('/pages/:page', help.page);
 
 
