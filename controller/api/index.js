7691b6d8292b49ccbcd98b3c4d87c3638f391338
Ilias Ismanalijev <hello@illyism.com>
2014-11-25 18:09:11 +0100

    disable api

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,22 +1,23 @@
-var checkName = require("./checkname.js");
-var who = require("./who.js");
+// var checkName = require("./checkname.js");
+// var who = require("./who.js");
 
 var api = {
 	checkName : function(req, res) {
-		var name = req.params["name"] || req.query["name"];
-		if (name == false) return res.jsonp({error: "No Name", status: -1});
-		checkName(name, function(result, status) {
-			res.jsonp({
-				name: name,
-				available: result,
-				status: status
-			})
-		});
+		res.jsonp({});
+		// var name = req.params["name"] || req.query["name"];
+		// if (name == false) return res.jsonp({error: "No Name", status: -1});
+		// checkName(name, function(result, status) {
+		// 	res.jsonp({
+		// 		name: name,
+		// 		available: result,
+		// 		status: status
+		// 	})
+		// });
 	},
 
 	who : function(req, req) {
 		res.jsonp({
-			count: who.users.length
+			count: 0//who.users.length
 		})
 	}
 }
