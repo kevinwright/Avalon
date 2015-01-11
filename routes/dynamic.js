a37b49acf3d8ae09dc7e45871a0414c30b4c80e4
Ilias Ismanalijev <hello@illyism.com>
2015-01-11 21:40:48 +0100

    watch changes

--- routes/dynamic.js
+++ routes/dynamic.js
@@ -4,7 +4,7 @@ var avalon = require("../controller/avalon");
 var fs = require("fs");
 
 var marked = require("meta-marked");
-var pages = require("//help/library/pages");
+var pages = [];
 
 function render(page) {
   return function (req, res) {
@@ -15,12 +15,32 @@ function render(page) {
   }
 }
 
+function watchChange(page) {
+  fs.watchFile(page.file, function(curr, prev) {
+    if (page.type == "library") {
+      fs.readFile(page.file, "utf8", function(err, content) {
+        if (err) return console.error(err);
+        page.library = marked(content)
+        console.log("text changed " + page.file);
+      });
+    } else if (page.type == "html") {
+      fs.readFile(page.file, "utf8", function(err, content) {
+        if (err) return console.error(err);
+        page.html = content
+        console.log("text changed " + page.file);
+      });
+    }
+  })
+}
+
 function addPage(page) {
   fs.readFile(page.file, "utf8", function(err, content) {
     if (err) return console.error(err);
     page.library = marked(content)
     router.get(page.url, render(page));
     console.log("Page added: "+page.url);
+    pages.push(page);
+    watchChange(page);
   });
 }
 
@@ -30,16 +50,22 @@ function addHtml(page) {
     page.html = content
     router.get(page.url, render(page));
     console.log("Page added: "+page.url);
+    pages.push(page);
+    watchChange(page);
   });
 }
 
-for (var i = 0; i<pages.length; i++) {
-  var page = pages[i];
-  if (page.type == "library") {
-    addPage(page);
-  } else if (page.type == "html") {
-    addHtml(page);
+function init() {
+  var files = require("/help/library/pages");
+  for (var i = 0; i<files.length; i++) {
+    var page = files[i];
+    if (page.type == "library") {
+      addPage(page);
+    } else if (page.type == "html") {
+      addHtml(page);
+    }
   }
 }
 
+init();
 module.exports = router;
