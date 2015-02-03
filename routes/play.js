9b7203c8086cd5e284bd59e40f650179c2acd87d
Ilias Ismanalijev <hello@illyism.com>
2015-02-03 17:21:04 +0100

    play library

--- routes/play.js
+++ routes/play.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 var avalon = require("../controller/avalon");
 var play = require("../controller/play");
-var avalon = require("../controller/avalon");
 
-router.get('/', function(req, res) {
-  res.render('play/index', {avalon:avalon});
-});
-router.get('/index.html', function(req, res) {
-  res.render('play/index', {avalon:avalon});
-});
-router.post('/', play.post);
+// Routes
+  router.get(['/', "/index.html"], getIndex);
+  router.post('/', play.post);
+
+// Methods
+  function getIndex(req, res, next) {
+    avalon.info("play.md", function(err, meta) {
+      if (err) return next(err);
+      res.render('play/index', {meta: meta.meta, avalon: avalon});
+    });
+  }
 
 module.exports = router;
