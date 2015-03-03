45f6f73d10dca663cd429e3fa829a48c544a1fdd
Ilias Ismanalijev <hello@illyism.com>
2015-03-03 15:17:57 +0100

    avatar

--- helper/sql.js
+++ helper/sql.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection(require("./sql.json"));
 
 module.exports = function(name, callback) {
-  connection.query("SELECT user.id, user.username, avatar.hash FROM  `dck_base_user` user INNER JOIN  `dck_base_avatar` avatar ON avatar.userId = user.id WHERE user.username =  ?? LIMIT 1",
+  connection.query("SELECT user.id, user.username, avatar.hash FROM  `dck_base_user` user INNER JOIN  `dck_base_avatar` avatar ON avatar.userId = user.id WHERE user.username =  ? LIMIT 1",
   [name],  
   function(err, rows, fields) {
     if (err) return callback(err);
