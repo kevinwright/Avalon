9c5729d89fcd487f705d168682b5f4ac1340756d
Ilias Ismanalijev <hello@illyism.com>
2015-03-03 17:16:07 +0100

    login

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,11 @@ var recent = require("../controller/news/recent.js");
   function getIndex(req, res, next) {
     recent(function(err, data) {
       avalon.info("front.md", function(err, meta, extra) {
-        if (err) return next(err);
-        res.render('index', { avalon: avalon, meta: meta.meta, extra:extra, recent: data });
+        avalon.info("play.md", function(err, play) {
+          if (err) return next(err);
+          meta.meta.form = play.meta.form;
+          res.render('index', { avalon: avalon, meta: meta.meta, extra:extra, recent: data });
+        });
       });
     })
   }
