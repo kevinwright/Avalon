2653c2432f7d41473b8435e9e8008d412a2c6dae
Ilias Ismanalijev <hello@illyism.com>
2015-03-03 15:14:02 +0100

    avatar

--- controller/api/avatar.js
+++ controller/api/avatar.js
@@ -1,10 +1,12 @@
-var avatars = require("/library/avatars.json");
 var request = require("request");
+var sql = require("../../helper/sql.js");
 
 module.exports = function(req, res, next) {
   var user = req.params["username"] || req.query["username"];
-  var avatar = avatars[user];
-  
-  var url = "http://www.meetplace.net/ow_userfiles/plugins/base/avatars/avatar_big_"+avatar.id+"_"+avatar.avatar+".jpg";
-  require('request').get(url).pipe(res); 
+  sql(user, function(err, avatar) {
+    if (err) return next(err);
+    
+    var url = "http://www.meetplace.net/ow_userfiles/plugins/base/avatars/avatar_big_"+avatar.id+"_"+avatar.hash+".jpg";
+    request.get(url).pipe(res); 
+  });
 }
