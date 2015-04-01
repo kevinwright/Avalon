7638ad4ca655d2f6c6e2ea17801caf149cea98a7
Ilias Ismanalijev <hello@illyism.com>
2015-04-01 12:39:20 +0200

    news

--- routes/news.js
+++ routes/news.js
@@ -2,17 +2,29 @@ var express = require('express');
 var router = express.Router();
 var avalon = require("../controller/avalon");
 var util = require("../helper/util");
+var recent = require("../controller/news/recent.js");
 
+var helpParser = require("../controller/help/parser");
+var HELPDIR = global.avalon.dir.help;
 
 // Methods
   function getIndex(req, res, next) {
-    avalon.info("news.md", function(err, meta) {
+    util.readFile(HELPDIR+"/"+"events", function(err, eventsFile) {
       if (err) return next(err);
-      util.renderYAML("/library/affairs", function(err, content) {
+      var eventsData = helpParser(eventsFile);
+      recent(function(err, recents) {
         if (err) return next(err);
-        res.render('news/index', {
-          meta: meta.meta,
-          affairs: content.affairs
+        avalon.info("news.md", function(err, meta) {
+          if (err) return next(err);
+          util.renderYAML("/library/affairs", function(err, content) {
+            if (err) return next(err);
+            res.render('news/index', {
+              meta: meta.meta,
+              affairs: content.affairs,
+              recent: recents,
+              events: eventsData
+            });
+          });
         });
       });
     });
