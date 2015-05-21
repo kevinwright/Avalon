6d9544fc44ac5e9c2cb3d0b9b89df07559134793
Kevin Wright <kev.lee.wright@gmail.com>
2015-05-21 18:21:29 +0100

    date style tweaks in news/events

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -29,13 +29,14 @@
     parts.forEach(function(name){
       var value = duration[name]();
       if(value !== 0) {
-        output.push(Math.abs(value) + " " + name);
+        output.push(Math.abs(value));
         namesused.push(name);
         past = past || (value < 0);
       }
     });
 
     output = output.slice(0,2);
+    namesused = namesused.slice(0,2);
     if(past) { output.push(" ago"); }
     var th = "<tr><th>" + output.join("</th><th>")+ "</th></tr>";
     var tr = "<tr><td>" + namesused.join("</td><td>")+ "</td></tr>";
