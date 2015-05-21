bd0283a2e5b1ccdd89e2e3762b5117fdfa1c8180
Kevin Wright <kev.lee.wright@gmail.com>
2015-05-21 12:18:47 +0100

    trying for dynamic countdown

---
+++ public/js/countdown.js
@@ -0,0 +1,51 @@
+(function(){
+
+  var currentLang = 'en',
+    snippets = [];
+
+  function updateSnippets () {
+    var i;
+
+    moment.locale(currentLang);
+
+    for (i = 0; i < snippets.length; i++) {
+      snippets[i].render();
+    }
+  }
+
+  function Snippet (el) {
+    this.el = el;
+    this.to = moment(el.attr("#data-to"));
+  }
+
+  var parts = ['days', 'hours', 'minutes', 'seconds'];
+
+  Snippet.prototype.render = function () {
+    var duration = moment.duration( timestamp.diff(moment().tz("Europe/London")) );
+    var output = [];
+
+    parts.forEach(function(name){
+      var value = duration[name]();
+      if(value > 0) {
+        output.push(value + " " + name);
+      }
+    });
+
+    output = output.slice(0,2);
+
+    this.el.html(output.join(', '));
+  };
+
+
+  function timedUpdate () {
+    updateSnippets();
+    setTimeout(timedUpdate, 1000);
+  }
+
+  $('.countdown').each(function () {
+    snippets.push(new Snippet($(this)));
+  });
+
+  timedUpdate();
+
+})();
