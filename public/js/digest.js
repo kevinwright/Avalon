fac826e9dedd4d31d04a46952085bf92fbbe5c26
Kevin Wright <kev.lee.wright@gmail.com>
2015-05-27 10:44:56 +0100

    Allow toggling-off for highlighted digest entries

--- public/js/digest.js
+++ public/js/digest.js
@@ -2,9 +2,15 @@
 
 
   $('.typelabel').click(function () {
-    var favoured = $(this).attr("data-type");
-    $('.entry').removeClass('emphasise').addClass('de-emphasise');
-    $('.type-'+favoured).removeClass('de-emphasise').addClass('emphasise');
+    var $this = $(this);
+    var favoured = $this.attr("data-type");
+    if($this.hasClass('emphasise')) {
+      //already highlighted
+      $('.entry').removeClass('emphasise').removeClass('de-emphasise');
+    } else {
+      $('.entry').removeClass('emphasise').addClass('de-emphasise');
+      $('.type-' + favoured).removeClass('de-emphasise').addClass('emphasise');
+    }
   });
 
 })();
